05cd5f1b57ae6e5929f29616e5af95f8cc2e45a9
김동현 <kimdh@pcninc.co.kr>
2021-07-09 17:49:35 +0900

    초기 파일 커밋

---
+++ srj-backend/src/main/java/com/srj/SrjBackendApplication.java
@@ -0,0 +1,13 @@
+package com.srj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrjBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrjBackendApplication.class, args);
+	}
+
+}
